6298f7fdd37e748b23b2749a4caa060ed567283f
isahua16 <isaelhuard@gmail.com>
2023-05-27 11:26:51 -0600

    playing with dictionaries and lists

--- data_types.py
+++ data_types.py
@@ -12,9 +12,43 @@ contains_test(str)
 contains_test(str2)
 
 nums = [1, 100, 200, 2, 9, 12]
+nums2 = [2, -1, 100]
 
-for num in nums:
-    if num > 10:
-        print("number is large", num)
-    elif num <= 10:
-        print("number is small", num)
+
+def check_numbers(arr):
+    for num in arr:
+        if num > 10:
+            print("number is large", num)
+        elif num <= 10:
+            print("number is small", num)
+
+
+check_numbers(nums)
+check_numbers(nums2)
+
+client = {
+    "username": "user_one",
+    "age": 30,
+    "friends": ["friend_one", "friend_two", "friend_three"],
+}
+
+print(client["username"], client["age"])
+
+for friend in client["friends"]:
+    print(friend)
+
+clients = [
+    {
+        "username": "user_one",
+        "age": 30,
+        "friends": ["friend_one", "friend_two", "friend_three"],
+    },
+    {
+        "username": "user_two",
+        "age": 25,
+        "friends": ["friend_four", "friend_five", "friend_six"],
+    },
+]
+
+for client in clients:
+    print(client["username"], client["age"], client["friends"])
